990ec99d7ce05fa3192c0d98749a9c81f836b7ce
nducros <nicolas.ducros@insa-lyon.fr>
2022-11-21 10:08:41 +0100

    128x128 reconstrucutions

--- 2022_OE_spyrit2/caption.py
+++ 2022_OE_spyrit2/caption.py
@@ -16,48 +16,83 @@ from pathlib import Path
 
 #%% user-defined
 img_size = 64
-M_list = [4095]#, 2048, 1024, 512, 256]
-N0_list = [10]#,10]
+M_list = [4095, 2048, 1024, 512, 256]
+N0_list = [10,2500]
 
 
 net_arch_list    = ['pinv-net', 'dc-net']
 net_denoi_list   = ['cnn','unet'] #['unet', 'cnn']    # 'cnn' 'cnnbn' or 'unet'
 net_data    = 'stl10'    # 'imagenet' or 'stl10'
 
-data_folder_list = [Path('usaf_x12/'), 
-                    Path('usaf_x2/'), 
-                    Path('star_sector_x2/'), 
-                    Path('star_sector_x12/'),
+data_folder_list = [Path('usaf_x2'), 
+                    Path('usaf_x12'),
+                    Path('star_sector_x2'), 
+                    Path('star_sector_x12'),
                     Path('star_sector_linear'),
-                    Path('tomato_slice_x2/'),
+                    Path('tomato_slice_x2'),
                     Path('tomato_slice_x12'),
-                    Path('cat/'),
-                    Path('cat_linear/'),
-                    Path('horse/')]
+                    Path('cat'),
+                    Path('cat_linear'),
+                    Path('horse')]
+
+sample_list = ['USAF target (×2 zoom)',
+               'USAF target (×12 zoom)',
+               'Siemens star (×2 zoom)', 
+               'Siemens star (×12 zoom)',
+               'Siemens star with linear variable filter',
+               'Tomato slice (×2 zoom)',
+               'Tomato slice (×12 zoom)',
+               'STL10 cat',
+               'STL10 cat with linear variable filter',
+               'STL10 horse']
+
 
 #%% Loop over compression ratios, network architectures, and image domain denoisers
 with open('caption.tex', 'w') as f:
-    for data_folder, M, N0, net_arch, net_denoi in [
-                                (data_folder, M, N0, net_arch, net_denoi) 
-                                for data_folder in data_folder_list
+    
+    # sample list
+    f.write('We consider the following samples\n')
+    f.write('\\begin{enumerate}\n')
+    for data_folder, sample in zip(data_folder_list,sample_list):
+        f.write(f'\\item {sample}, see Sec. \\ref{{sec:{data_folder.name}}}\n')    
+    f.write('\end{enumerate}')
+    f.write('\n\n')
+    
+    # Network list
+    f.write('We consider the following network\n')
+    f.write('\\begin{enumerate}\n')
+    for data_folder, sample in zip(data_folder_list,sample_list):
+        f.write(f'\\item {sample}, see \\ref{{sec:{data_folder.name}}}\n')    
+    f.write('\end{enumerate}')
+    f.write('\n\n')
+    
+    for data_folder, sample in zip(data_folder_list,sample_list):
+        
+        text = f'''\subsection{{{sample}}}
+        \label{{sec:{data_folder.name}}}\n'''
+        f.write(text)
+        f.write('\n')
+        
+        for M, N0, net_arch, net_denoi in [
+                                (M, N0, net_arch, net_denoi) 
                                 for M in M_list 
                                 for N0 in N0_list
                                 for net_arch in net_arch_list 
                                 for net_denoi in net_denoi_list]:   
     
-        net_suffix  = f'N0_{N0}_N_64_M_{M}_epo_30_lr_0.001_sss_10_sdr_0.5_bs_1024_reg_1e-07'
-        net_title = f'{net_arch}_{net_denoi}_{net_data}_{net_suffix}'
-        
-        full_path_slice = (data_folder.name + '_slice_' + net_title)
-        full_path_bin = (data_folder.name + '_bin_' + net_title)
-        print(full_path_bin)
-        
-        text = f'''
-        \\begin{{center}}
-            \includegraphics[width=\linewidth]{{{{{full_path_slice}}}.pdf}}
-            \includegraphics[width=\linewidth]{{{{{full_path_bin}}}.pdf}}
-            \captionof{{figure}}{{Reconstruction of height spectral channels (top) and spectral bins (bottom) using a Pinv-Net trained at $N0 = {N0}$ photons for $M = {M}$ measurements. The image domain denoiser is a {net_denoi}.}}
-        \end{{center}}'''#.format(full_path_slice, full_path_bin)
-        
-        f.write(text)
-        f.write('\n\n')
+            net_suffix  = f'N0_{N0}_N_64_M_{M}_epo_30_lr_0.001_sss_10_sdr_0.5_bs_1024_reg_1e-07'
+            net_title = f'{net_arch}_{net_denoi}_{net_data}_{net_suffix}'
+            
+            full_path_slice = (data_folder.name + '_slice_' + net_title)
+            full_path_bin = (data_folder.name + '_bin_' + net_title)
+            print(full_path_bin)
+            
+            text = f'''
+            \\begin{{center}}
+                \includegraphics[width=\linewidth,trim={{0 2.7cm 0 2.5cm}},clip]{{{{{full_path_bin}}}.pdf}}
+                \includegraphics[width=\linewidth,trim={{0 2.7cm 0 2.9cm}},clip]{{{{{full_path_slice}}}.pdf}}
+                \captionof{{figure}}{{Reconstruction of four spectral bins (top) and channels (bottom) using a {net_arch} trained at $\\alpha = {N0}$ photons for $M = {M}$ measurements. The image domain denoiser is a {net_denoi}.}}
+            \end{{center}}'''#.format(full_path_slice, full_path_bin)
+            
+            f.write(text)
+            f.write('\n\n')
--- 2022_OE_spyrit2/mask_cov_128.py
+++ 2022_OE_spyrit2/mask_cov_128.py
@@ -3,28 +3,101 @@
 import numpy as np
 from spyrit.misc.statistics import Cov2Var, img2mask
 from matplotlib import pyplot as plt
-from spyrit.misc.disp import add_colorbar
+from spyrit.misc.disp import add_colorbar, noaxis
+
+from pathlib import Path
+
 
 #%%
-M = [1024,2048,4096]
+M = [4096, 1024, 512] #[1024, 4095, 4096]
+N = 128
+
+title_Tag = False
 
 C = np.load('../../stat/ILSVRC2012_v10102019/Cov_8_128x128.npy')
+save_root = Path('./recon_128/')
+
+#%% Energy/variance subsampling
 Ord = Cov2Var(C)
 mask_0 = img2mask(Ord, M[0])
 mask_1 = img2mask(Ord, M[1])
 mask_2 = img2mask(Ord, M[2])
 
-fig , axs = plt.subplots(1,3)
-#noaxis(axs)
-#
-im = axs[0].imshow(mask_0, cmap='gray')
-axs[0].set_title(f"M = {M[0]}")
-add_colorbar(im)
-#
-im = axs[1].imshow(mask_1, cmap='gray')
-axs[1].set_title(f"M = {M[1]}")
-add_colorbar(im)
-#
-im = axs[2].imshow(mask_2, cmap='gray')
-axs[2].set_title(f"M = {M[2]}")
-add_colorbar(im)
+#%% Square subsampling
+Ord = np.zeros((N,N))
+K = int(M[0]**.5)
+M_act = K**2
+Ord[:K,:K]= -np.arange(-M_act,0).reshape(K,K)
+mask_3 = img2mask(Ord, M[0])
+
+Ord = np.zeros((N,N))
+K = int(M[1]**.5)
+M_act = K**2
+Ord[:K,:K]= -np.arange(-M_act,0).reshape(K,K)
+mask_4 = img2mask(Ord, M[1])
+
+Ord = np.zeros((N,N))
+K = int(M[2]**.5)
+M_act = K**2
+Ord[:K,:K]= -np.arange(-M_act,0).reshape(K,K)
+mask_5 = img2mask(Ord, M[2])
+
+#%%
+fig , axs = plt.subplots(1,1)
+im = axs.imshow(mask_0, cmap='gray')
+if title_Tag:
+    axs.set_title(f"M = {M[0]}")
+noaxis(axs)
+
+full_path = save_root / ( f'mask_var_M_{M[0]}'+ '.png')
+fig.savefig(full_path, bbox_inches='tight')
+
+#%%
+fig , axs = plt.subplots(1,1)
+im = axs.imshow(mask_1, cmap='gray')
+if title_Tag:
+    axs.set_title(f"M = {M[1]}")
+noaxis(axs)
+
+full_path = save_root / ( f'mask_var_M_{M[1]}'+ '.png')
+fig.savefig(full_path, bbox_inches='tight')
+
+#%%
+fig , axs = plt.subplots(1,1)
+im = axs.imshow(mask_2, cmap='gray')
+if title_Tag:
+    axs.set_title(f"M = {M[2]}")
+noaxis(axs)
+
+full_path = save_root / ( f'mask_var_M_{M[2]}'+ '.png')
+fig.savefig(full_path, bbox_inches='tight')
+
+#%%
+fig , axs = plt.subplots(1,1)
+im = axs.imshow(mask_3, cmap='gray')
+if title_Tag:
+    axs.set_title(f"M = {M[0]}")
+noaxis(axs)
+
+full_path = save_root / ( f'mask_rect_M_{M[0]}'+ '.png')
+fig.savefig(full_path, bbox_inches='tight')
+
+#%%
+fig , axs = plt.subplots(1,1)
+im = axs.imshow(mask_4, cmap='gray')
+if title_Tag:
+    axs.set_title(f"M = {M[1]}")
+noaxis(axs)
+
+full_path = save_root / ( f'mask_rect_M_{M[1]}'+ '.png')
+fig.savefig(full_path, bbox_inches='tight')
+
+#%%
+fig , axs = plt.subplots(1,1)
+im = axs.imshow(mask_5, cmap='gray')
+if title_Tag:
+    axs.set_title(f"M = {M[2]}")
+noaxis(axs)
+
+full_path = save_root / ( f'mask_rect_M_{M[2]}'+ '.png')
+fig.savefig(full_path, bbox_inches='tight')
