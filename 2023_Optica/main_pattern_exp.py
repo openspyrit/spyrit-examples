5573009debb07fdf33a93a19f07f9ed7dcab996a
nducros <nicolas.ducros@insa-lyon.fr>
2023-06-13 11:32:40 +0200

    Plot and save experimental patterns

--- 2023_Optica/main_pattern_exp.py
+++ 2023_Optica/main_pattern_exp.py
@@ -18,8 +18,10 @@ H_exp = np.load(Path(mat_folder) / f'motifs_Hadamard_{M}_{N}.npy')
 H_exp = np.flip(H_exp,1).copy() # copy() required to remove negatives strides
 H_exp /= H_exp[0,16:500].mean()
 
+H_exp2 = H_exp/H_exp[0,:]
+
 # save
-np.save(Path(save_folder) / 'patterns_2023_03_13.npy', H_exp)
+#np.save(Path(save_folder) / 'patterns_2023_03_13.npy', H_exp)
 
 #%% Load experimental measurement matrix (after split compensation)
 from pathlib import Path
@@ -31,7 +33,7 @@ M,N = H_exp.shape
 H_tar = walsh_matrix(N)
 H_tar = H_tar[:M]
 
-#%% plot
+#%% plot 2D
 f, axs = plt.subplots(2, 1)
 axs[0].set_title('Target measurement patterns')
 im = axs[0].imshow(H_tar, cmap='gray') 
@@ -42,3 +44,20 @@ axs[1].set_title('Experimental measurement patterns')
 im = axs[1].imshow(H_exp, cmap='gray') 
 add_colorbar(im, 'bottom')
 axs[1].get_xaxis().set_visible(False)
+
+#%% plot 1d
+fig_folder = './figure/'
+
+ind = [[0,1,2],[6,12,46]]
+
+f, axs = plt.subplots(2, 3, figsize=(15,7))
+for ax, ix in zip(axs, ind):
+    for a,i in zip(ax, ix):
+        print(i)
+        print(a)
+        a.plot(H_tar[i,:])
+        a.plot(H_exp[i,:])
+        a.plot(H_exp2[i,:])
+        a.set_title(f'Patterns #{i}')
+        
+plt.savefig(Path(fig_folder) / 'patterns_profiles', bbox_inches='tight', dpi=600)
