5ba80aad2d9e59284a545cf3c37e4409c421ae2b
nducros <nicolas.ducros@insa-lyon.fr>
2024-01-11 18:30:12 +0100

    Update unmixing scripts

--- 2023_Optica/main_unmix_filter_EGFP-DsRed_14.py
+++ 2023_Optica/main_unmix_filter_EGFP-DsRed_14.py
@@ -11,15 +11,14 @@ import matplotlib.pyplot as plt
 from fonction.matrix_tools import smooth
 
 #%% Load reference spectrum
-EGFP_path = "./data/Reference_spectra/EGFP_fpbase_spectra_500_608.csv"
-DsRed2_path = "./data/Reference_spectra/DsRed2_fpbase_spectra_500_608.csv"
-DsRed_path = "./data/Reference_spectra/DsRed_fpbase_spectra_500_608.csv"
-DsRed_exp_path = "./data/Reference_spectra/DsRed_express_fpbase_spectra_500_6088.csv"#
+notch_path = "./data/Reference_spectra/ZET532NF_transmission.csv"
+EGFP_path = "./data/Reference_spectra/EGFP_fpbase_spectra.csv"
+DsRed2_path = "./data/Reference_spectra/DsRed2_fpbase_spectra.csv"
 
-data_DsRed_exp = np.genfromtxt(DsRed_exp_path, delimiter=',',skip_header=True)
-# data_DsRed = np.genfromtxt(DsRed_path, delimiter=',',skip_header=True)
-# data_DsRed2 = np.genfromtxt(DsRed2_path, delimiter=',',skip_header=True)
-data_EGFP = np.genfromtxt(EGFP_path, delimiter=',',skip_header=True)
+
+data_DsRed = np.genfromtxt(DsRed2_path, delimiter=',',skip_header=True)
+data_EGFP  = np.genfromtxt(EGFP_path, delimiter=',',skip_header=True)
+data_notch = np.genfromtxt(notch_path, delimiter=';',skip_header=True)
 
 # on utilise les spectres expérimentaux. 
 # La DsRed est en réalité de la DsRed2
@@ -29,29 +28,25 @@ Nc = 128 # number of channels
 # Interpolate spectra to have Nc channels
 from scipy import interpolate
 
-L_lambda = np.linspace(500, 608, Nc)
+#L_lambda = np.linspace(500, 608, Nc)
+L_lambda = np.linspace(488, 610, Nc)
 f = interpolate.interp1d(data_EGFP[:,0], data_EGFP[:,1], kind='cubic')
 L_EGFP = f(L_lambda)   
-f = interpolate.interp1d(data_DsRed_exp[:,0], data_DsRed_exp[:,1], kind='cubic')
+f = interpolate.interp1d(data_DsRed[:,0], data_DsRed[:,1], kind='cubic')
 L_DsRed_exp = f(L_lambda)
-
-# Notch filter @ 513 nm
-Filt_notch = np.ones(Nc)
-Filt_notch[40:57] = np.zeros(17)
-Filt_notch = smooth(Filt_notch,6)
-Filt_notch[:4] = np.ones(4)
-Filt_notch[128-4:] = np.ones(4)
+f = interpolate.interp1d(data_notch[:,0], data_notch[:,1], kind='cubic')
+L_notch = f(L_lambda)
 
 # plot
 fig, axs = plt.subplots()
 axs.plot(L_lambda, L_DsRed_exp, label='DsRed') 
 axs.plot(L_lambda, L_EGFP, label='EGFP')
-axs.plot(L_lambda, Filt_notch, label='Notch filter')
+axs.plot(L_lambda, L_notch, label='Notch filter')
 axs.set_xlabel('wavelength (nm)')
 axs.set_title('Spectra used for unmixing')
 axs.legend()
 
-#%%
+#%% Load hypercube
 from pathlib import Path
 
 # Load data
@@ -61,7 +56,8 @@ Nc = 128 # number of channels
 
 T_list = range(1,27)    # slice indices
 load_path = './data/2023_03_13_2023_03_14_eGFP_DsRed_3D'
-recon = 'tikhonet50_div1.5'  # 'pinv' 'tikhonet50_div1.5'
+suffix = ''               # with leading underscore e.g. '_shift' '_registered'
+recon = 'tikhonet50_div1.5' + suffix         # 'pinv'  'tikhonet50_div1.5'
 
 # all slices are unmixed jointly!
 Nz = T_list.stop - T_list.start + 1
@@ -86,6 +82,39 @@ for z, t in enumerate(T_list):
 # Replace negative values by zeros
 # xyzl_cube = np.where(xyzl_cube > 0, xyzl_cube, 0)
 
+#%% Double check pinv recon from pilot
+# from pathlib import Path
+
+# # Load data
+# Nl = 512 # number of pixcels along the y dimensions 
+# Nh = 512 # number of measured Walsh_Hadmard coefficients (correpond to the h dimensions)
+# Nc = 128 # number of channels
+
+# T_list = range(1,27)    # slice indices
+# load_path = './data/2023_03_13_2023_03_14_eGFP_DsRed_3D'
+
+# # all slices are unmixed jointly!
+# Nz = T_list.stop - T_list.start + 1
+# xyzl_cube = np.zeros((Nl,Nh,Nz,Nc))
+
+# # loop over z-slices
+# for z, t in enumerate(T_list):
+#     if t<6:
+#         date = '2023_03_13'
+#         Run = f'RUN{t+1:04}'
+#     else:
+#         date = '2023_03_14'
+#         Run = f'RUN{t-5:04}'
+    
+#     filename = f'T{t}_{Run}_{date}_Had_rc_pinv_{Nl}x{Nh}x{Nc}.npy'
+#     print(filename)
+#     xyl_cube = np.load(Path(load_path) / 'Reconstruction/hyper_cube_pilot' / filename)
+    
+#     xyzl_cube[:,:,z,:] = xyl_cube
+    
+# Replace negative values by zeros
+# xyzl_cube = np.where(xyzl_cube > 0, xyzl_cube, 0)
+
 #%% Estimate auto-fluorescence spectrum
 from matplotlib.patches import Rectangle
 
@@ -95,6 +124,7 @@ y1,y2 = 335,355
 z1,z2 = 1,5
 
 spec_fluo = np.mean(xyzl_cube[x1:x2, y1:y2, z1:z2,:], axis=(0,1,2))
+spec_fluo /= np.max(spec_fluo)
 
 # plot autofluorescence area
 fig, axs = plt.subplots(1, z2-z1, figsize=(2*(z2-z1),z2-z1))
@@ -107,6 +137,51 @@ for i, z in enumerate(range(z1,z2)):
     axs[i].add_patch(Rectangle((y1, x1), y2-y1+1, x2-x1+1, linewidth=1, edgecolor='r', facecolor='none'))
     
 fig.suptitle(f'Autofluorescence estimation area; x in [{x1}-{x2}]; y in [{y1}-{y2}]')
+
+
+
+fig, axs = plt.subplots()
+axs.plot(L_lambda, L_DsRed_exp, '--r',  label='DsRed (theo)') 
+axs.plot(L_lambda, L_EGFP*L_notch, '--g',  label='EGFP x Notch (theo)') 
+axs.plot(L_lambda, L_notch, '--k',  label='Notch (theo)') 
+axs.plot(L_lambda, spec_fluo, color=(1,0.5, 0),  label='autofluo (estimated)')
+axs.legend()
+
+#%% Estimate fluorescence spectra
+from matplotlib.patches import Rectangle
+
+z = 4
+# Area where the dsre spectrum is estimated
+x1_r,x2_r = 221,226
+y1_r,y2_r = 232,237
+
+# Area where the egfp spectrum is estimated
+x1_g,x2_g = 321+16,330+16
+y1_g,y2_g = 367,372
+
+L_DsRed_pure = np.mean(xyzl_cube[x1_r:x2_r, y1_r:y2_r, z,:], axis=(0,1))
+L_DsRed_pure /= L_DsRed_pure.max() 
+L_EGFP_pure = np.mean(xyzl_cube[x1_g:x2_g, y1_g:y2_g, z,:], axis=(0,1))
+L_EGFP_pure /= L_EGFP_pure.max() 
+
+# plot area
+fig, axs = plt.subplots(1, 2)
+axs[0].imshow(xyzl_cube[:,:,z,64])
+axs[0].set_xlabel('y-axis')
+axs[0].set_ylabel('x-axis')
+# Create a Rectangle patch
+axs[0].add_patch(Rectangle((y1_r, x1_r), y2_r-y1_r+1, x2_r-x1_r+1, linewidth=2, edgecolor='r', facecolor='none'))
+axs[0].add_patch(Rectangle((y1_g, x1_g), y2_g-y1_g+1, x2_g-x1_g+1, linewidth=2, edgecolor='g', facecolor='none'))
+    
+
+axs[1].plot(L_lambda, L_DsRed_pure, 'r', label='DsRed (expe)')
+axs[1].plot(L_lambda, L_DsRed_exp, '--r',  label='DsRed (theo)') 
+axs[1].plot(L_lambda, L_EGFP_pure, 'g', label='EGFP (expe)')
+axs[1].plot(L_lambda, L_EGFP*L_notch, '--g',  label='EGFP x Notch (theo)') 
+axs[1].plot(L_lambda, L_notch, '--k',  label='Notch (theo)') 
+axs[1].legend()
+
+fig.suptitle(f'DsRed estimation area; x in [{x1_r}-{x2_r}]; y in [{y1_r}-{y2_r}]')
 
     
 #%% Manual registration (compensate for sample shift during acquisition)
@@ -126,18 +201,23 @@ xyzl_cube_reg[:, :, 13:, :] = xyzl_cube[x_shift:,      y_shift:,      13:,  :]
 from pysptools import abundance_maps
 
 member_list = ['DsRed','EGFP','Autofluo'] #['DsRed','EGFP','Autofluo','Noise']
-unmixing_folder = '/Unmixing_2/'
+unmix = 'calib_blind' + suffix # 'calib_blind_' 'calib_'
+unmixing_folder = '/Unmixing' + '_' + unmix + '/'
 
 method_unmix = 'NNLS' # 'NNLS''_UCLS'
 Nm = 4
 Nl,Nh = xyzl_cube_reg.shape[:2] # new shape after registration
 
+# blind region (removed from unmixing)
+blind_start = 518   # in nm
+blind_end = 547     # in nm
+ind = np.where((L_lambda < blind_start ) | (L_lambda > blind_end))
+
 # End member (spectra) matrix
 U = np.zeros((Nm,Nc))
-U[0] = L_DsRed_exp*Filt_notch#
-U[1] = L_EGFP*Filt_notch #L_mRFP
-U[2] = spec_fluo/np.max(spec_fluo) # spectre d'autofluo normalisé
-U[3] = np.ones(128)*1/10 
+U[0] = L_DsRed_exp*L_notch#
+U[1] = L_EGFP*L_notch #L_mRFP
+U[2] = spec_fluo # spectre d'autofluo normalisé
 
 folder_unmix = recon + '_' + method_unmix
 
@@ -146,33 +226,41 @@ print('-- unmixing')
 # measurment matrix with all pixels across rows
 M = np.reshape(xyzl_cube_reg, (-1,Nc)) 
 
+# Remove blind region
+U_blind = U[:,ind[0]]
+M_blind = M[:,ind[0]]
+
 # https://pysptools.sourceforge.io/abundance_maps.html
 if method_unmix == 'NNLS':
     # Non-negative Constrained Least Squares
-    Abondance = abundance_maps.amaps.NNLS(M, U)
+    Abondance = abundance_maps.amaps.NNLS(M_blind, U_blind)
     
 elif method_unmix == 'UCLS':
     # Unconstrained Least Squares
-    Abondance = abundance_maps.amaps.UCLS(M, U)
+    Abondance = abundance_maps.amaps.UCLS(M_blind, U_blind)
 
 abond_unmix_4D = np.reshape(Abondance,(Nl,Nh,Nz,Nm))
 
 #%% Separate fluorophores simulating two band-pass filters
 Nf = 2 # number of filter
 
-filtering_folder = '/Filtering_2/'
-
+filtering_folder = '/Filtering' + '_' + unmix + '/'
 filter_list = ['green','red']
-l_green_1, l_green_2 =  6,  18 # green filter band
-l_red_1,   l_red_2   = 99, 111 # red filter band
+
+c_egfp = 510  # in nm
+c_dsred = 600    # in nm
+bandwidth = 10 # in nm
+
 
 folder_filter = recon # 
 
 print('-- filtering')
 
 Filt_sep = np.zeros((Nf,Nc))
-Filt_sep[0,l_green_1:l_green_2] = 1
-Filt_sep[1,l_red_1:l_red_2] = 1
+Filt_sep[0] = np.where((L_lambda < c_egfp + bandwidth/2)&(L_lambda > c_egfp - bandwidth/2), 
+                    1, 0)
+Filt_sep[1] = np.where((L_lambda < c_dsred + bandwidth/2)&(L_lambda > c_dsred - bandwidth/2), 
+                    1, 0)
 
 abond_filter_4D = M @ Filt_sep.T
 abond_filter_4D = abond_filter_4D.reshape((Nl,Nh,Nz,Nf))
@@ -197,7 +285,7 @@ if save_tag:
     
     # save members/spectra  
     filename = 'spectra.npy'
-    np.save(save_path / filename, U)
+    np.save(save_path / filename, U_blind)
 
 # filtering
 if save_tag:    
@@ -226,6 +314,14 @@ if save_fig:
     if not os.path.exists(save_path):
         os.makedirs(save_path)
         
+if save_fig:
+    filename = 'spectra_pure_pixels'
+    for ext in save_ext:
+        filename_with_ext = filename + '.' + ext
+        print(filename_with_ext)
+        plt.savefig(save_path / filename_with_ext, bbox_inches='tight', dpi=600)
+    plt.close(fig)  
+        
 # Abondance maps
 # loop over all z-slices
 for z, t in enumerate(T_list):
@@ -250,13 +346,16 @@ for z, t in enumerate(T_list):
         plt.close(fig)
 
 # unmixing: spectra
-col = ['r', 'g', 'b', 'c']  # color list
+col = ['r', 'g', (1,0.5, 0)]  # color list
+
+L_lambda_blind = L_lambda[ind[0]]
 
 fig, axs = plt.subplots()
 for m in range(len(member_list)):
-    axs.plot(L_lambda, U[m], col[m], label = member_list[m])
-axs.plot(L_lambda, Filt_notch, 'k', ls = 'dashed', label = 'notch filter')
-axs.fill_between(L_lambda, Filt_notch, 1, color='k', alpha=.1)
+    axs.plot(L_lambda_blind, U_blind[m], color=col[m], label = member_list[m])
+    
+axs.plot(L_lambda, L_notch, 'k', ls = 'dashed', label = 'notch filter')
+axs.fill_between(L_lambda, L_notch, 1, color='k', alpha=.1)
 axs.legend()
 plt.xlabel('Wavelenght (nm)')
 plt.ylabel('Intensity (normalized)')
@@ -328,8 +427,8 @@ fig, axs = plt.subplots()
 axs.plot(L_lambda, L_DsRed_exp, 'r', label = member_list[0])
 axs.plot(L_lambda, L_EGFP, 'g', label = member_list[1])
 axs.plot(L_lambda, U[2], 'b', label = member_list[2])
-axs.plot(L_lambda, Filt_notch, 'k', ls = 'dashed', label = 'notch filter')
-axs.fill_between(L_lambda, Filt_notch, 1, color='k', alpha=.1)
+axs.plot(L_lambda, L_notch, 'k', ls = 'dashed', label = 'notch filter')
+axs.fill_between(L_lambda, L_notch, 1, color='k', alpha=.1)
 axs.plot(L_lambda, Filt_sep[0], 'g', ls = 'dashdot', label = 'green filter')
 axs.fill_between(L_lambda, Filt_sep[0], 0, color='g', alpha=.1)
 axs.plot(L_lambda, Filt_sep[1], 'r', ls = 'dashdot', label = 'red filter')
--- 2023_Optica/main_unmix_filter_mRFP-DsRed.py
+++ 2023_Optica/main_unmix_filter_mRFP-DsRed.py
@@ -10,32 +10,29 @@ import os
 import matplotlib.pyplot as plt
 
 #%% Load reference spectrum
-DsRed_exp_path = "./data/Reference_spectra//DsRed_express_fpbase_spectra_550_658.csv" 
-mCherry_path = "./data/Reference_spectra//mCherry_fpbase_spectra_550_658.csv"
-mRFP_path =  "./data/Reference_spectra//mRFP1_fpbase_spectra_550_658.csv"
+DsRed_path = "./data/Reference_spectra//DsRed2_fpbase_spectra.csv" 
+mRFP_path =  "./data/Reference_spectra//mRFP1_fpbase_spectra.csv"
 
-data_DsRed_exp = np.genfromtxt(DsRed_exp_path, delimiter=';',skip_header=True)#,
-data_mCherry = np.genfromtxt(mCherry_path, delimiter=';',skip_header=True)
+data_DsRed = np.genfromtxt(DsRed_path, delimiter=',',skip_header=True)#,
 data_mRFP = np.genfromtxt(mRFP_path, delimiter=';',skip_header=True)
+
+mRFP_path =  "./data/Reference_spectra//mRFP12_fpbase_spectra.csv"
+data_mRFP = np.genfromtxt(mRFP_path, delimiter=',',skip_header=True)
+
+suffix = 'calib_mrfp12'
 
 Nc = 128 # number of channels
 
 # Interpolate spectra to have Nc channels
 from scipy import interpolate
 
-L_lambda = np.linspace(550, 658, Nc)
+#L_lambda = np.linspace(550, 658, Nc)
+L_lambda = np.linspace(538, 660, Nc)
 f = interpolate.interp1d(data_mRFP[:,0], data_mRFP[:,1], kind='cubic')
 L_mRFP = f(L_lambda)   
-f = interpolate.interp1d(data_DsRed_exp[:,0], data_DsRed_exp[:,1], kind='cubic')
-L_DsRed_exp = f(L_lambda)
+f = interpolate.interp1d(data_DsRed[:,0], data_DsRed[:,1], kind='cubic')
+L_DsRed = f(L_lambda)
 
-# plot
-fig, axs = plt.subplots()
-axs.plot(L_lambda, L_DsRed_exp, label='DsRed') 
-axs.plot(L_lambda, L_mRFP, label='mRFP')
-axs.set_xlabel('wavelength (nm)')
-axs.set_title('Spectra used for unmixing')
-axs.legend()
 
 #%%
 from pathlib import Path
@@ -47,7 +44,7 @@ Nc = 128 # number of channels
 
 T_list = [*range(4, 8), *range(9, 25)] # slice indices, Run0008 corrupted
 load_path = './data/2023_02_28_mRFP_DsRed_3D'
-recon = 'tikhonet50_div1.5'  # 'pinv' 'tikhonet50_div1.5'
+recon = 'pinv'  # 'pinv' 'tikhonet50_div1.5' 'pinv_shift' 'tikhonet50_div1.5_shift'
 
 # all slices are unmixed jointly!
 Nz = len(T_list)
@@ -71,11 +68,12 @@ for z, t in enumerate(T_list):
 from matplotlib.patches import Rectangle
 
 # Area where the autofluorescence is estimated, which corresponds to the "sac vitellin"
-x1,x2 = 235-50,285-50
+x1,x2 = 235-30,285-30
 y1,y2 = 320,345
 z1,z2 = 0,4  # ['RUN0004','RUN0005','RUN0006']
 
-spec_fluo = np.mean(xyzl_cube[x1:x2, y1:y2, z1:z2,:], axis=(0,1,2))
+L_fluo = np.mean(xyzl_cube[x1:x2, y1:y2, z1:z2,:], axis=(0,1,2))
+L_fluo /= np.max(L_fluo)
 
 # plot autofluorescence area
 for z in range(z1,z2):
@@ -87,11 +85,50 @@ for z in range(z1,z2):
     # Create a Rectangle patch
     axs.add_patch(Rectangle((y1, x1), y2-y1+1, x2-x1+1, linewidth=1, edgecolor='r', facecolor='none'))
     
+    
+#%% Estimate fluorescence spectra
+from matplotlib.patches import Rectangle
+
+z = 14  #window
+dx = 3  #window
+
+# Area where the mrfp spectrum is estimated
+x1_r,x2_r = 269-dx,269+dx
+y1_r,y2_r = 380-dx,380+dx
+
+# Area where the dsred spectrum is estimated
+x1_g,x2_g = 414-dx,414+dx
+y1_g,y2_g = 401-dx,401+dx
+
+# Integrate
+L_mRFP_pure = np.mean(xyzl_cube[x1_r:x2_r, y1_r:y2_r, z,:], axis=(0,1))
+L_DsRed_pure = np.mean(xyzl_cube[x1_g:x2_g, y1_g:y2_g, z,:], axis=(0,1))
+L_mRFP_pure /= L_mRFP_pure.max() 
+L_DsRed_pure /= L_DsRed_pure.max() 
+
+# plot area
+fig, axs = plt.subplots(1, 2)
+axs[0].imshow(xyzl_cube[:,:,z,64])
+axs[0].set_xlabel('y-axis')
+axs[0].set_ylabel('x-axis')
+# Create a Rectangle patch
+axs[0].add_patch(Rectangle((y1_r, x1_r), y2_r-y1_r+1, x2_r-x1_r+1, linewidth=2, edgecolor='r', facecolor='none'))
+axs[0].add_patch(Rectangle((y1_g, x1_g), y2_g-y1_g+1, x2_g-x1_g+1, linewidth=2, edgecolor='g', facecolor='none'))
+    
+# Plot spectra
+axs[1].plot(L_lambda, L_DsRed_pure, 'm', label='DsRed (expe)')
+axs[1].plot(L_lambda, L_DsRed, '--m',    label='DsRed (theo)') 
+axs[1].plot(L_lambda, L_mRFP_pure, 'r',  label='mRFP (expe)')
+axs[1].plot(L_lambda, L_mRFP, '--r',     label='mRFP (theo)') 
+axs[1].legend()
+
+fig.suptitle(f'DsRed estimation area; x in [{x1_r}-{x2_r}]; y in [{y1_r}-{y2_r}]')
+    
 
 #%% Unmixing
 from pysptools import abundance_maps
 
-unmixing_folder =  '/Unmixing_3/'
+unmixing_folder =  '/Unmixing_' + suffix + '/'
 member_list = ['DsRed','mRFP','Autofluo']  # member_list = ['DsRed','mRFP','Autofluo','Noise'] 
 
 method_unmix = 'NNLS' # 'NNLS''_UCLS'
@@ -100,9 +137,9 @@ Nl,Nh = xyzl_cube.shape[:2] # new shape after registration
 
 # End member (spectra) matrix
 U = np.zeros((Nm,Nc))
-U[0] = L_DsRed_exp
+U[0] = L_DsRed
 U[1] = L_mRFP 
-U[2] = spec_fluo/np.max(spec_fluo) # spectre d'autofluo normalisé
+U[2] = L_fluo # spectre d'autofluo normalisé
 U[3] = np.ones(128)*1/10 
 
 unmixing_subfolder = recon + '_' + method_unmix
@@ -126,18 +163,29 @@ abond_unmix_4D = np.reshape(Abondance,(Nl,Nh,Nz,Nm))
 #%% Separate fluorophores simulating two band-pass filters
 Nf = 2 # number of filter
 
-filtering_folder = '/Filtering_3/'
+filtering_folder = '/Filtering' + '_' + suffix + '/'
 filter_list = ['orange','red']
 l_green_1, l_green_2 =  40, 52 # green filter band
 l_red_1,   l_red_2   = 62, 74 # red filter band
+
+c_dsred = 570  # in nm
+c_mrfp = 620    # in nm
+bandwidth = 10 # in nm
 
 filtering_subfolder = recon # 
 
 print('-- filtering')
 
+
 Filt_sep = np.zeros((Nf,Nc))
-Filt_sep[0,l_green_1:l_green_2] = 1
-Filt_sep[1,l_red_1:l_red_2] = 1
+Filt_sep[0] = np.where((L_lambda < c_dsred + bandwidth/2)&(L_lambda > c_dsred - bandwidth/2), 
+                    1, 0)
+Filt_sep[1] = np.where((L_lambda < c_mrfp + bandwidth/2)&(L_lambda > c_mrfp - bandwidth/2), 
+                    1, 0)
+
+# Filt_sep = np.zeros((Nf,Nc))
+# Filt_sep[0,l_green_1:l_green_2] = 1
+# Filt_sep[1,l_red_1:l_red_2] = 1
 
 abond_filter_4D = M @ Filt_sep.T
 abond_filter_4D = abond_filter_4D.reshape((Nl,Nh,Nz,Nf))
@@ -190,7 +238,15 @@ if save_fig:
     print(save_path)
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-        
+
+if save_fig:
+    filename = 'spectra_pure_pixels'
+    for ext in save_ext:
+        filename_with_ext = filename + '.' + ext
+        print(filename_with_ext)
+        plt.savefig(save_path / filename_with_ext, bbox_inches='tight', dpi=600)
+    plt.close(fig)        
+
 # Abondance maps
 # loop over all z-slices
 for z, t in enumerate(T_list):
@@ -211,15 +267,15 @@ for z, t in enumerate(T_list):
         for ext in save_ext:
             filename_with_ext = filename + '.' + ext
             print(filename_with_ext)
-            plt.savefig(save_path / filename, bbox_inches='tight', dpi=600)
+            plt.savefig(save_path / filename_with_ext, bbox_inches='tight', dpi=600)
         plt.close(fig)
 
 # unmixing: spectra
-col = ['r', 'g', 'b', 'c']  # color list
+col = ['m', 'r', (1,0.5, 0)]  # color list
 
 fig, axs = plt.subplots()
 for m in range(len(member_list)):
-    axs.plot(L_lambda, U[m], col[m], label = member_list[m])
+    axs.plot(L_lambda, U[m], color=col[m], label = member_list[m])
 axs.legend()
 plt.xlabel('Wavelenght (nm)')
 plt.ylabel('Intensity (normalized)')
@@ -289,12 +345,12 @@ for z, t in enumerate(T_list):
         
 # Spectra
 fig, axs = plt.subplots()
-axs.plot(L_lambda, L_DsRed_exp, 'r', label = member_list[0])
-axs.plot(L_lambda, L_mRFP, 'g', label = member_list[1])
-axs.plot(L_lambda, U[2], 'b', label = member_list[2])
-axs.plot(L_lambda, Filt_sep[0], 'g', ls = 'dashdot', label = 'green filter')
+axs.plot(L_lambda, L_DsRed, 'm', label = member_list[0])
+axs.plot(L_lambda, L_mRFP, 'r', label = member_list[1])
+axs.plot(L_lambda, U[2], color=(1,0.5, 0), label = member_list[2])
+axs.plot(L_lambda, Filt_sep[0], 'm', ls = 'dashdot', label = 'DsRed filter')
 axs.fill_between(L_lambda, Filt_sep[0], 0, color='g', alpha=.1)
-axs.plot(L_lambda, Filt_sep[1], 'r', ls = 'dashdot', label = 'red filter')
+axs.plot(L_lambda, Filt_sep[1], 'r', ls = 'dashdot', label = 'mRFP filter')
 axs.fill_between(L_lambda, Filt_sep[1], 0, color='r', alpha=.1)
 axs.legend()
 plt.xlabel('Wavelenght (nm)')
