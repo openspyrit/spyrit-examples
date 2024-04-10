ca28a9a234854d12e0d026770ee7e1d586e9f501
jabascal <juanabascal78@gmail.com>
2024-04-10 11:11:09 +0200

    add metrics on sim test set and clean up

--- tutorial/fig_recon_sim_prelim.py
+++ tutorial/fig_recon_sim_prelim.py
@@ -22,9 +22,13 @@ import pickle
 import collections
 collections.Callable = collections.abc.Callable
 
+from skimage.metrics import structural_similarity as ssim
+from skimage.metrics import peak_signal_noise_ratio as psnr
+from skimage.metrics import normalized_root_mse as nrmse
+
 import torchvision
 
-from spyrit.misc.statistics import Cov2Var
+from spyrit.misc.statistics import Cov2Var, data_loaders_ImageNet
 from spyrit.core.noise import Poisson 
 from spyrit.core.meas import HadamSplit
 from spyrit.core.prep import SplitPoisson
@@ -34,14 +38,13 @@ from spyrit.core.nnet import Unet, ConvNet, Identity
 from spyrit.misc.sampling import reorder, Permutation_Matrix
 from spyrit.misc.disp import add_colorbar, noaxis
 
-
 from spas import read_metadata, spectral_slicing
 
 torch.manual_seed(0)
 
 #%% user-defined
 # used for acquisition
-N_acq = 64
+N_acq = 32
 
 # for reconstruction
 N_rec = 128  # 128 or 64
@@ -53,8 +56,11 @@ N0 = 10     # Check if we used 10 in the paper
 stat_folder_rec = Path('../../stat/oe_paper/') # Path('../../stat/ILSVRC2012_v10102019/')
 
 mode_sim = True # Reconstruct simulated images in addition to exp
-mode_exp = True
+mode_exp = False
 mode_sim_crop = False
+mode_eval_metrics = True
+metrics_eval = ['nrmse', 'ssim', 'psnr'] 
+num_batchs_metrics = 1 # Number of batchs to evaluate
 
 # Reconstruction parameters
 metrics = False # Compute metrics: MSE
@@ -78,12 +84,13 @@ path_natural_images = '../../../../Nextcloud/spyrit_slides/spie/figures/target/'
 # Select image
 img_id = None # None: all images; 0: first image
 
-net_data    = 'imagenet'    # 'imagenet'
-
-bs = 256
+bs = 64
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+# Path for data evaluation
+data_root = '../../data/ILSVRC2012_v10102019/'
+net_data    = 'imagenet'    # 'imagenet'
 # ---------------------------------------------------------
 # Models
 # (net_arch, net_denoi, other_specs). 
@@ -138,7 +145,7 @@ models_specs = [
                 },                
                 ]
 
-models_specs = models_specs[1:]
+models_specs = models_specs[0:]
 
 ######################################################
 # Reconstruction functions
@@ -192,13 +199,6 @@ def init_reconstruction_network(noise, prep, Cov_rec, net_arch, net_denoi = None
     model.to(device)
     return model, denoi
 
-def compute_mse(x, x_gt):
-    if isinstance(x, np.ndarray):
-        #torch.linalg.norm(x - x_gt)/ 
-        mse = np.linalg.norm(x - x_gt)
-        mse = np.array(mse)/np.linalg.norm(x_gt)
-    return mse
-
 # Reconstruction: set attributes and reconstruct
 def reconstruct(model, y, device, log_fidelity= False, step_estimation = False, wls = False, metrics = False):
     with torch.no_grad():
@@ -224,7 +224,7 @@ def reconstruct(model, y, device, log_fidelity= False, step_estimation = False, 
         # Metrics 
         if metrics:
             rec_sim = rec_sim_gpu.cpu().detach().numpy()
-            mse = compute_mse(rec_sim, x_gt)
+            mse = compute_nrmse(rec_sim, x_gt)
         
         rec_sim = rec_sim_gpu.cpu().detach().numpy().squeeze()
         rec_sim = rec_sim.reshape(b, N_rec, N_rec)
@@ -270,6 +270,60 @@ def sampling_order(N_rec, M, net_order):
         Ord_rec = Cov2Var(Cov_rec)
     return Ord_rec
 
+def data_loader(data_root_train, data_root_val, img_size, batch_size):
+    # Data loaders
+    dataloaders = data_loaders_ImageNet(data_root_train, 
+                                data_root_val, 
+                                img_size=img_size, 
+                                batch_size=batch_size)
+    return dataloaders['val']
+
+def compute_nrmse(x, x_gt):
+    if isinstance(x, np.ndarray):
+        nrmse_val = nrmse(x, x_gt)
+    else:
+        nrmse_val = torch.linalg.norm(x - x_gt)/ torch.linalg.norm(x_gt)
+    return nrmse_val
+
+def compute_pnsr(x, x_gt):
+    psnr_val = psnr(x, x_gt, data_range=x_gt.max() - x_gt.min())
+    return psnr_val
+
+def compute_ssim(x, x_gt):
+    ssim_val = ssim(x, x_gt, data_range=x_gt.max() - x_gt.min())
+    return ssim_val
+
+def evaluate_model(model, dataloader, device, metrics = ['nrmse', 'ssim', 'psnr'], num_batchs = None):
+    # Evaluate
+    model.eval()
+    results = {}
+    for i, (inputs, _) in enumerate(dataloader):
+        if num_batchs is not None and i >= num_batchs:
+            break
+        inputs = inputs.to(device)
+        outputs = model(inputs)
+        inputs = inputs.cpu().detach().numpy().squeeze()
+        outputs = outputs.cpu().detach().numpy().squeeze()
+        mse_val = []
+        ssim_val = []
+        psnr_val = []
+        if 'nrmse' in metrics:
+            mse_batch = compute_nrmse(outputs, inputs)
+            mse_val.append(mse_batch)
+        if 'ssim' in metrics:
+            ssim_batch = compute_ssim(outputs, inputs)
+            ssim_val.append(ssim_batch)
+        if 'psnr' in metrics:
+            psnr_batch = compute_pnsr(outputs, inputs)
+            psnr_val.append(psnr_batch)
+    if 'nrmse' in metrics:
+        results['nrmse'] = mse_val
+    if 'ssim' in metrics:
+        results['ssim'] = ssim_val
+    if 'psnr' in metrics:
+        results['psnr'] = psnr_val
+    return results   
+
 # ---------------------------------------------------------
 #%% Parameters for simulated images 
 def transform_gray_norm(img_size, crop_type = 'center'): 
@@ -309,7 +363,58 @@ def transform_gray_norm_rand_crop(img_size, seed = 0, shuffle = True):
     )    
     return transform
 
+# ---------------------------------------------------------
+#%% Simulated images
+if mode_sim:
+    # Create dataset and loader (expects class folder 'images/test/')
+    #from spyrit.misc.statistics import transform_gray_norm
+    if mode_sim_crop:
+        transform = transform_gray_norm_rand_crop(N_rec)
+    else:
+        transform = transform_gray_norm(N_rec)
+
+    # Check if the directory exists
+    if not os.path.isdir(path_natural_images):
+        print(f"Directory does not exist: {path_natural_images}")
+    dataset = torchvision.datasets.ImageFolder(root=path_natural_images, transform=transform)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size = 7)
+
+    x, _ = next(iter(dataloader))
+    print(f'Shape of input images: {x.shape}')
+
+    # Select image
+    if img_id is not None:
+        x = x[img_id:img_id+1,:,:,:]
+    x = x.detach().clone()
+    b,c,h,w = x.shape
+    # Uncomment for MSE for unroll iterations: for mode_eval_metrics = False
+    # x_gt = np.copy(x)
+
+    # plot
+    for i in range(b):
+        x_plot = x[i].view(-1,h,h).cpu().numpy() 
+        if b > 1:
+            name_save_sim = f'sim{i}_{N_rec}_gt'
+        else:
+            name_save_sim = f'sim_{N_rec}_gt'
+        if mode_sim_crop:
+            name_save_sim = name_save_sim + '_crop'
+        full_path = save_root / (name_save_sim + '.png')
+
+        if i == 0:
+            imshow_specs(x_plot[0,:,:], vmin=-1, vmax=0.4, colorbar=colorbar, path_save=full_path)
+        else:
+            imshow_specs(x_plot[0,:,:], vmin=vmin, vmax=vmax, colorbar=colorbar, path_save=full_path)
+
 ######################################################
+# Data to evaluate metrics
+if mode_eval_metrics:
+    dataloader_val = data_loader(data_root_train=Path(data_root) / 'test', 
+                                   data_root_val=Path(data_root) / 'val', 
+                                   img_size=N_rec, 
+                                   batch_size=bs)
+    results_metrics = {}
+
 # Loop over models
 for model_specs in models_specs:
     # Model specifications
@@ -323,11 +428,17 @@ for model_specs in models_specs:
     step_decay = other_specs.get('step_decay', None)
     wls = other_specs.get('wls', False)
     step_grad = other_specs.get('step_grad', False)
+    x_gt = None
     # DRUNet
     noise_level = other_specs.get('noise_level', None)
 
-    # Clear model to save memory
-    model, denoi = None, None 
+    # Free memory
+    if 'model' in locals():
+        model.to('cpu')
+        del model
+    if 'denoi' in locals():
+        denoi.to('cpu')
+        del denoi
 
     # Name save
     name_save_details = f'{net_arch}_{net_denoi}'
@@ -338,50 +449,7 @@ for model_specs in models_specs:
         if step_decay != 1:
             name_save_details = name_save_details + f'_sdec{step_decay}'.replace('.','')
         if step_grad:
-            name_save_details = name_save_details + '_sgrad'        
-
-    # ---------------------------------------------------------
-    #%% Simulated images
-    if mode_sim:
-        # Create dataset and loader (expects class folder 'images/test/')
-        #from spyrit.misc.statistics import transform_gray_norm
-        if mode_sim_crop:
-            transform = transform_gray_norm_rand_crop(N_rec)
-        else:
-            transform = transform_gray_norm(N_rec)
-
-        # Check if the directory exists
-        if not os.path.isdir(path_natural_images):
-            print(f"Directory does not exist: {path_natural_images}")
-        dataset = torchvision.datasets.ImageFolder(root=path_natural_images, transform=transform)
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size = 7)
-
-        x, _ = next(iter(dataloader))
-        print(f'Shape of input images: {x.shape}')
-
-        # Select image
-        if img_id is not None:
-            x = x[img_id:img_id+1,:,:,:]
-        x = x.detach().clone()
-        b,c,h,w = x.shape
-        x_gt = np.copy(x)
-
-        # plot
-        for i in range(b):
-            x_plot = x[i].view(-1,h,h).cpu().numpy() 
-            if b > 1:
-                name_save_sim = f'sim{i}_{N_rec}_gt'
-            else:
-                name_save_sim = f'sim_{N_rec}_gt'
-            if mode_sim_crop:
-                name_save_sim = name_save_sim + '_crop'
-            full_path = save_root / (name_save_sim + '.png')
-
-            if i == 0:
-                imshow_specs(x_plot[0,:,:], vmin=-1, vmax=0.4, colorbar=colorbar, path_save=full_path)
-            else:
-                imshow_specs(x_plot[0,:,:], vmin=vmin, vmax=vmax, colorbar=colorbar, path_save=full_path)
-
+            name_save_details = name_save_details + '_sgrad'   
     # ---------------------------------------------------------
     #%% covariance matrix and network filnames
     if N_rec==64:
@@ -428,7 +496,13 @@ for model_specs in models_specs:
         if net_denoi == 'drunet':
             denoi.set_noise_level(noise_level)
             name_save_details = name_save_details + f'_nlevel_{noise_level}'
-        ###########################################################################       
+        ###########################################################################   
+        # Evaluate metrics
+        if mode_eval_metrics:
+            results = evaluate_model(model, dataloader_val, device, metrics = metrics_eval, num_batchs = num_batchs_metrics)
+            print(f'Metrics for {name_save_details}: {results}')
+            results_metrics[name_save_details] = results
+        # ---------------------------------------------------------    
         #%% simulations
         if mode_sim:
             x = x.view(b * c, h * w)
@@ -524,6 +598,7 @@ for model_specs in models_specs:
                         imshow_specs(rec, vmin=None, vmax=None, colorbar=colorbar, path_save=full_path)
               
                     
+print(f'Metrics for {name_save_details}: {results}')
             
                     
                 
