ca85ae9ce016dba1f85be8f52bc4b9136b667d1b
romainphan <romainphan.rp@gmail.com>
2024-10-11 14:02:39 +0200

    change to non-deprecated nn.init.kaiming.normal_

--- 2024_Optics_Express/utility_dpgd.py
+++ 2024_Optics_Express/utility_dpgd.py
@@ -134,9 +134,9 @@ class DFBNet(nn.Module):
 def weights_init_kaiming(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
-        nn.init.kaiming_normal(m.weight.data, a=0, mode='fan_in')
+        nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
     elif classname.find('Linear') != -1:
-        nn.init.kaiming_normal(m.weight.data, a=0, mode='fan_in')
+        nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
     elif classname.find('BatchNorm') != -1:
         m.weight.data.normal_(mean=0, std=math.sqrt(2./9./64.)).clamp_(-0.025,0.025)
         nn.init.constant(m.bias.data, 0.0)
