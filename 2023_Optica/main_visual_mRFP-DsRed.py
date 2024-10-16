b19c6242b1612f99923e25f9c83b502a35f93b28
nducros <nicolas.ducros@insa-lyon.fr>
2024-10-16 10:16:05 +0200

    change colors and folder structure

--- 2023_Optica/main_visual_mRFP-DsRed.py
+++ 2023_Optica/main_visual_mRFP-DsRed.py
@@ -17,8 +17,8 @@ Nc = 128 # number of channels
 load_path = Path('./data/2023_02_28_mRFP_DsRed_3D')
 T_list = range(1,27)    # slice indices
 
-suffix = '_calib_mrfp12_shift' # '', '_calib_mrfp1_shift', '_calib_mrfp12_shift'
-recon = 'pinv_shift'  # 'pinv_shift', 'tikhonet50_div1.5_shift'
+suffix = '_calib_mrfp1_shift'      # '', '_calib_mrfp1_shift', '_calib_mrfp12_shift'
+recon = 'tikhonet50_div1.5_shift'   # 'pinv_shift', 'tikhonet50_div1.5_shift'
 method_unmix = 'NNLS' # 'NNLS''_UCLS'
 
 member_list = ['DsRed','mRFP','Autofluo'] #['Dsred','mRFP','AF']
@@ -38,39 +38,25 @@ print(fullpath)
 abond_filter_4D = np.load(fullpath / 'abondance.npy')
 abond_filter_4D = np.rot90(abond_filter_4D, k=2, axes=(0, 1))
 
-#%%
-def colorize(im, color, clip_percentile=0.1):
-    """
-    Helper function to create an RGB image from a single-channel image using a 
-    specific color.
-    """
-    # Check that we just have a 2D image
-    if im.ndim > 2 and im.shape[2] != 1:
-        raise ValueError('This function expects a single-channel image!')
-        
-    # Rescale the image according to how we want to display it
-    im_scaled = im.astype(np.float32) - np.percentile(im, clip_percentile)
-    im_scaled = im_scaled / np.percentile(im_scaled, 100 - clip_percentile)
-    print(f'Norm: min={np.percentile(im, clip_percentile)}, max={np.percentile(im_scaled, 100 - clip_percentile)}')
-    print(f'New:  min={im_scaled.min()}, max={im_scaled.max()}')
-    im_scaled = np.clip(im_scaled, 0, 1)
-    
-    # Need to make sure we have a channels dimension for the multiplication to work
-    im_scaled = np.atleast_3d(im_scaled)
-    
-    # Reshape the color (here, we assume channels last)
-    color = np.asarray(color).reshape((1, 1, -1))
-    return im_scaled * color
 
 #%% Visualize in color (could be merged with the SPIM image)
 # from https://bioimagebook.github.io/chapters/1-concepts/4-colors/python.html
+
 import matplotlib.pyplot as plt
 import os
+from utility import colorize
 
 save_tag = True
 
 z_list = [*range(4, 8), *range(9, 25)] # slice indices, Run0008 corrupted
-sat = 0.15 # in percentage
+
+sat = 0.15              # percentage of saturated pixel
+color_unmix_1 = (1, 0, 1)
+color_unmix_2 = (0, 1, 1)
+color_unmix_3 = (.5, .5, 0)
+color_filt_1 = color_unmix_1
+color_filt_2 = color_unmix_2
+
 
 # loop over all z-slices
 for z, t in enumerate(z_list):
@@ -79,9 +65,9 @@ for z, t in enumerate(z_list):
     test = abond_unmix_4D[:,:,z,0:3]
     
     # The color we provide gives RGB values in order, between 0 and 1
-    im_unmix_1 = colorize(test[..., 0], (1, 0, 1), sat)
-    im_unmix_2 = colorize(test[..., 1], (0, 1, 1), sat)
-    im_unmix_3 = colorize(test[..., 2], (0.5, 0.5, 0), sat)
+    im_unmix_1 = colorize(test[..., 0], color_unmix_1, sat)
+    im_unmix_2 = colorize(test[..., 1], color_unmix_2, sat)
+    im_unmix_3 = colorize(test[..., 2], color_unmix_3, sat)
     im_unmix = im_unmix_1 + im_unmix_2 #+ im_unmix_3
     #im_unmix /= im_unmix.max()
     
@@ -90,8 +76,8 @@ for z, t in enumerate(z_list):
     test = np.clip(test, 0, None)
     
     # The color we provide gives RGB values in order, between 0 and 1
-    im_filt_1 = colorize(test[..., 0], (1, 0, 1), sat)
-    im_filt_2 = colorize(test[..., 1], (0, 1, 1), sat)
+    im_filt_1 = colorize(test[..., 0], color_filt_1, sat)
+    im_filt_2 = colorize(test[..., 1], color_filt_2, sat)
     im_filt = im_filt_1 + im_filt_2 
     
     # plot
@@ -126,13 +112,20 @@ for z, t in enumerate(z_list):
         if not os.path.exists(save_path):
             os.makedirs(save_path)
         
-        filename = f'T{t}.svg'
+        filename = f'T{t:02}_abondance.svg'
         print(filename)
         plt.savefig(save_path / filename, bbox_inches='tight', dpi=600)
         plt.close(fig)
         
 #%% plot all quantitative maps independently
 save_tag = True
+
+sat = 0.15              # percentage of saturated pixel
+color_unmix_1 = (1, 0, 0)
+color_unmix_2 = (0, 1, 1)
+color_unmix_3 = (.5, .5, 0) # (1, .5, 0.15)
+color_filt_1 = color_unmix_1
+color_filt_2 = color_unmix_2
 
 # loop over all z-slices
 for z, t in enumerate(z_list):
@@ -142,22 +135,22 @@ for z, t in enumerate(z_list):
     im_unmix_all = np.zeros(test.shape + (3,)) # add an RGB dimension
     
     # The color we provide gives RGB values in order, between 0 and 1
-    im_unmix_all[...,0,:] = colorize(test[..., 0], (1, 0, 1), sat)
-    im_unmix_all[...,1,:] = colorize(test[..., 1], (0, 1, 1), sat)
-    im_unmix_all[...,2,:] = colorize(test[..., 2], (0.5, 0.5, 0), sat)
+    im_unmix_all[...,0,:] = colorize(test[..., 0], color_unmix_1, sat)
+    im_unmix_all[...,1,:] = colorize(test[..., 1], color_unmix_2, sat)
+    im_unmix_all[...,2,:] = colorize(test[..., 2], color_unmix_3, sat)
     
     if save_tag:
-        save_path = load_path / ('Visualisation' + suffix) / folder_unmix / f'T{t}'
+        save_path = load_path / ('Visualisation' + suffix) / folder_unmix / 'qmap'
         print(save_path)
         if not os.path.exists(save_path):
             os.makedirs(save_path)
             
         for m in range(3):
-            plt.imsave(save_path / f'qmap_{m}.png', im_unmix_all[...,m,:])
-        plt.imsave(save_path / 'qmap_12.png', np.clip(im_unmix_all[...,0,:] + im_unmix_all[...,1,:], 0, 1))
-        plt.imsave(save_path / 'qmap_23.png', np.clip(im_unmix_all[...,1,:] + im_unmix_all[...,2,:], 0, 1))
-        plt.imsave(save_path / 'qmap_13.png', np.clip(im_unmix_all[...,0,:] + im_unmix_all[...,2,:], 0, 1))
-        plt.imsave(save_path / 'qmap_123.png', np.clip(im_unmix_all[...,0,:] + im_unmix_all[...,1,:] + im_unmix_all[...,2,:], 0, 1))
+            plt.imsave(save_path / f'qmap_{m}_T{t:02}.png', im_unmix_all[...,m,:])
+        plt.imsave(save_path / f'qmap_01_T{t:02}.png', np.clip(im_unmix_all[...,0,:] + im_unmix_all[...,1,:], 0, 1))
+        plt.imsave(save_path / f'qmap_12_T{t:02}.png', np.clip(im_unmix_all[...,1,:] + im_unmix_all[...,2,:], 0, 1))
+        plt.imsave(save_path / f'qmap_02_T{t:02}.png', np.clip(im_unmix_all[...,0,:] + im_unmix_all[...,2,:], 0, 1))
+        plt.imsave(save_path / f'qmap_012_T{t:02}.png', np.clip(im_unmix_all[...,0,:] + im_unmix_all[...,1,:] + im_unmix_all[...,2,:], 0, 1))
 
 #%% plot all filter maps fmaps independently
 
@@ -170,18 +163,18 @@ for z, t in enumerate(z_list):
     im_filter_all = np.zeros(test.shape + (3,))
     
     # The color we provide gives RGB values in order, between 0 and 1
-    im_filter_all[...,0,:] = colorize(test[..., 0], (1, 0, 1), sat)
-    im_filter_all[...,1,:] = colorize(test[..., 1], (0, 1, 1), sat)
+    im_filter_all[...,0,:] = colorize(test[..., 0], color_filt_1, sat)
+    im_filter_all[...,1,:] = colorize(test[..., 1], color_filt_2, sat)
     
     if save_tag:
-        save_path = load_path / ('Visualisation' + suffix) / folder_unmix / f'T{t}'
+        save_path = load_path / ('Visualisation' + suffix) / folder_unmix / 'fmap'
         print(save_path)
         if not os.path.exists(save_path):
             os.makedirs(save_path)
             
         for m in range(2):
-            plt.imsave(save_path / f'fmap_{m}.png', im_filter_all[...,m,:])
-        plt.imsave(save_path / 'fmap_12.png', np.clip(im_filter_all[...,0,:] + im_filter_all[...,1,:], 0, 1))
+            plt.imsave(save_path / f'fmap_{m}_T{t:02}.png', im_filter_all[...,m,:])
+        plt.imsave(save_path / f'fmap_01_T{t:02}.png', np.clip(im_filter_all[...,0,:] + im_filter_all[...,1,:], 0, 1))
 
 
 #%% This was an attempt with microplot (not really better that using matplotlb directly)
@@ -191,3 +184,25 @@ for z, t in enumerate(z_list):
 # test = np.moveaxis(test, -1, 0)
 # microim = microplot.microshow(images=test, cmaps=['pure_cyan', 'pure_green', 'pure_green'])
 # microim = microplot.microshow(images=[test[0], test[1]], cmaps=['pure_cyan', 'YlGn'])
+
+#%% Upload to pilot-warehouse
+from pathlib import Path 
+import girder_client as gc
+
+# api Rest url of the warehouse
+url='https://pilot-warehouse.creatis.insa-lyon.fr/api/v1'
+
+
+# Generate the warehouse client
+gc = gc.GirderClient(apiUrl=url)
+
+# Authentification
+txt_file = open(Path('C:/Users/ducros/.apikey/pilot-warehouse.txt'), 'r', encoding='utf8')
+apiKey = txt_file.read()
+gc.authenticate(apiKey=apiKey)  # Authentication to the warehouse
+
+
+folder = load_path  / ('Visualisation' + suffix)
+
+folderId = '66ff9c49ae27f5ad8259f38a' # /2023_02_28_mRFP_DsRed_3D/
+gc.upload(str(folder), folderId, reuseExisting=True)
