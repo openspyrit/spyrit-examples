951f9be00d6a04577f2db738d22048c36a5ee02b
romainphan <romainphan.rp@gmail.com>
2025-01-09 14:05:08 +0100

    reformatting aux_functions

--- 2024_spyrit/aux_functions.py
+++ 2024_spyrit/aux_functions.py
@@ -4,7 +4,7 @@ Auxiliary functions for the Optics Express 2024 paper.
 
 import torch
 import numpy as np
-import matplotlib.pyplot as plt	
+import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
 from skimage.metrics import normalized_root_mse as nrmse
@@ -12,15 +12,17 @@ from skimage.metrics import structural_similarity as ssim
 
 
 # DISPLAY FUNCTIONS
-def imagesc_mod(img,
-               title='',
-               figsize=(5, 5),
-               colormap=plt.cm.gray,
-               title_fontsize=16,
-               dpi=100,
-               minmax=None,
-               showscale=False,
-               **kwargs):
+def imagesc_mod(
+    img,
+    title="",
+    figsize=(5, 5),
+    colormap=plt.cm.gray,
+    title_fontsize=16,
+    dpi=100,
+    minmax=None,
+    showscale=False,
+    **kwargs,
+):
     """
     Plot images with a custom colormap and a custom color for 'nan' values.
     """
@@ -33,43 +35,43 @@ def imagesc_mod(img,
     if minmax is None:
         minmax = (img[~img.isnan()].min(), img[~img.isnan()].max())
     # define the color for 'nan' values
-    colormap.set_bad(color='grey')
+    colormap.set_bad(color="grey")
     plt.imshow(img, cmap=colormap, vmin=minmax[0], vmax=minmax[1], **kwargs)
     plt.title(title, fontsize=title_fontsize)
     if showscale:
         # cax = plt.axes([0.85, 0.1, 0.075, 0.8])
         plt.colorbar(orientation="vertical")
     plt.show()
-    
+
 
 def split_meas2img(measurements, meas_operator):
     r"""
     Generates a 2D image from split measurements acquired from a LinearSplit or
     HadamSplit operator.
-    
+
     /!\ The measurements must be in the alternating positive / negative format.
-    
+
     Using spyrit 2.3.2
     """
     M = meas_operator.M
     N = meas_operator.N
-    h,w = meas_operator.meas_shape
+    h, w = meas_operator.meas_shape
     # using 'nan' so that we can show them with a custom color (see imagesc_mod)
-    img_pos = torch.full((N,), torch.tensor(float('nan'))) # even rows
-    img_neg = torch.full((N,), torch.tensor(float('nan'))) # odd rows
-    
+    img_pos = torch.full((N,), torch.tensor(float("nan")))  # even rows
+    img_neg = torch.full((N,), torch.tensor(float("nan")))  # odd rows
+
     # split the measurements in pos/neg, then apply meas2img to each
-    meas = measurements.view(2*M)
+    meas = measurements.view(2 * M)
     meas_pos = meas[0::2]
     meas_neg = meas[1::2]
-    
+
     # fill img_pos and img_neg with the measurements
     img_pos[meas_operator.indices[:M]] = meas_pos
     img_neg[meas_operator.indices[:M]] = meas_neg
-    
+
     # concatenate and reshape the images
-    img = torch.cat((img_pos.reshape(h,w), img_neg.reshape(h,w)), dim=0)
-    
+    img = torch.cat((img_pos.reshape(h, w), img_neg.reshape(h, w)), dim=0)
+
     return img
 
 
@@ -83,12 +85,14 @@ def center_measurements(measurements):
     return measurements - (max_val + min_val) / 2
 
 
-def compute_nrmse(x, x_gt, dim=[2,3]):
+def compute_nrmse(x, x_gt, dim=[2, 3]):
     # Compute relative error across pixels
     if isinstance(x, np.ndarray):
         nrmse_val = nrmse(x, x_gt)
     else:
-        nrmse_val = torch.linalg.norm(x - x_gt, dim=dim)/ torch.linalg.norm(x_gt, dim=dim)
+        nrmse_val = torch.linalg.norm(x - x_gt, dim=dim) / torch.linalg.norm(
+            x_gt, dim=dim
+        )
     return nrmse_val
 
 
@@ -102,32 +106,34 @@ def compute_ssim(x, x_gt):
     return torch.tensor(ssim_val)
 
 
-def compute_metric_batch(images, targets, metric='nrmse', operation='sum'):
+def compute_metric_batch(images, targets, metric="nrmse", operation="sum"):
     """
     Compute mean and variance of a metric
     """
-    if metric == 'nrmse':
+    if metric == "nrmse":
         metric_batch = compute_nrmse(images, targets)
-    elif metric == 'ssim':
+    elif metric == "ssim":
         metric_batch = compute_ssim(images, targets)
     else:
-        raise ValueError(f'Metric {metric} not supported')
-    
-    if operation == 'sum':
+        raise ValueError(f"Metric {metric} not supported")
+
+    if operation == "sum":
         # Sum over all images in the batch
         metric_batch_sum = torch.sum(metric_batch)
-        
+
         # Sum of squares to compute variance
         metric_batch_sq = torch.sum(metric_batch**2)
         return metric_batch_sum, metric_batch_sq
-    elif operation == 'mean':
+    elif operation == "mean":
         metric_batch = torch.mean(metric_batch)
         return metric_batch
     else:
-        raise ValueError(f'Operation {operation} not supported')
+        raise ValueError(f"Operation {operation} not supported")
 
 
-def eval_model_metrics_batch_cum(model, dataloader, device, metrics = ['nrmse', 'ssim'], num_batchs = None):
+def eval_model_metrics_batch_cum(
+    model, dataloader, device, metrics=["nrmse", "ssim"], num_batchs=None
+):
     """
     Compute metrics meand and variance for a dataset, accumulating across batches
     """
@@ -141,14 +147,21 @@ def eval_model_metrics_batch_cum(model, dataloader, device, metrics = ['nrmse', 
         outputs = model(inputs)
         for metric in metrics:
             # Accumulate sum and sum of squares across batches
-            results_batch_sum, results_batch_sq = compute_metric_batch(outputs, inputs, metric)
-            results[metric] = results.get(metric, 0)  + results_batch_sum.cpu().detach().numpy().item()
-            results[metric + '_var'] = results.get(metric + '_var', 0) + results_batch_sq.cpu().detach().numpy().item()
+            results_batch_sum, results_batch_sq = compute_metric_batch(
+                outputs, inputs, metric
+            )
+            results[metric] = (
+                results.get(metric, 0) + results_batch_sum.cpu().detach().numpy().item()
+            )
+            results[metric + "_var"] = (
+                results.get(metric + "_var", 0)
+                + results_batch_sq.cpu().detach().numpy().item()
+            )
 
         n = n + inputs.shape[0]
     for metric in metrics:
         # Compute mean and variance
         results[metric] = results[metric] / n
-        results[metric + '_var'] = results[metric + '_var'] / n - results[metric]**2
-        results[metric + '_std_dev'] = results[metric + '_var']**0.5
-    return results   
+        results[metric + "_var"] = results[metric + "_var"] / n - results[metric] ** 2
+        results[metric + "_std_dev"] = results[metric + "_var"] ** 0.5
+    return results
