67c8d28b648b7a94395768c63c56ea6d5ac28d63
romainphan <romainphan.rp@gmail.com>
2025-01-10 15:47:54 +0100

    formatted fig S7

--- 2024_spyrit/supplemental_figure_S7.py
+++ 2024_spyrit/supplemental_figure_S7.py
@@ -1,4 +1,4 @@
-# %% 
+# %%
 # Imports
 # --------------------------------------------------------------------
 import ast
@@ -17,19 +17,19 @@ import spyrit.misc.sampling as samp
 import spyrit.external.drunet as drunet
 
 
-# %% 
+# %%
 # General
 # --------------------------------------------------------------------
 # Experimental data
-data_folder = 'data/'              # measurements
-model_folder = 'model/'             # reconstruction models
-stat_folder  = 'stat/'              # statistics
-recon_folder = 'recon/supplemental_figure_S7/'    # reconstructed images
+data_folder = "data/"  # measurements
+model_folder = "model/"  # reconstruction models
+stat_folder = "stat/"  # statistics
+recon_folder = "recon/supplemental_figure_S7/"  # reconstructed images
 
 # Full paths
 data_folder_full = Path.cwd() / Path(data_folder)
 model_folder_full = Path.cwd() / Path(model_folder)
-stat_folder_full  = Path.cwd() / Path(stat_folder)
+stat_folder_full = Path.cwd() / Path(stat_folder)
 recon_folder_full = Path.cwd() / Path(recon_folder)
 recon_folder_full.mkdir(parents=True, exist_ok=True)
 
@@ -40,10 +40,7 @@ data_title = [
     "tomato_slice_2_zoomx12",
     "tomato_slice_2_zoomx2",
 ]
-suffix = {
-    "data": "_spectraldata.npz",
-    "metadata": "_metadata.json"
-}
+suffix = {"data": "_spectraldata.npz", "metadata": "_metadata.json"}
 n_meas = len(data_title)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -52,49 +49,49 @@ img_size = 128
 subsampling_factor = 2
 
 
-# %% 
+# %%
 # Measurement (and reconstruction) operators
 # ====================================================================
 # number of measurements
-M = (img_size // subsampling_factor)**2
+M = (img_size // subsampling_factor) ** 2
 
 # Measurement and noise operators
 Ord_rec = np.ones((img_size, img_size))
-Ord_rec[:,img_size//2:] = 0
-Ord_rec[img_size//2:,:] = 0
+Ord_rec[:, img_size // 2 :] = 0
+Ord_rec[img_size // 2 :, :] = 0
 
 meas_op = meas.HadamSplit(M, img_size, torch.from_numpy(Ord_rec))
-noise_op = noise.Poisson(meas_op, 2) # parameter alpha is unimportant here
-prep_op = prep.SplitPoisson(2, meas_op) # same here
+noise_op = noise.Poisson(meas_op, 2)  # parameter alpha is unimportant here
+prep_op = prep.SplitPoisson(2, meas_op)  # same here
 
 
-# %% 
+# %%
 # Load experimental data and prepare it for reconstruction
 # ====================================================================
 print("Looking for data in", data_folder_full)
 
 # Collect data in numpy
 exp_data = [
-    np.load(data_folder_full / (title + suffix["data"]))['spectral_data']
+    np.load(data_folder_full / (title + suffix["data"]))["spectral_data"]
     for title in data_title
 ]
 # Collect metadata
 patterns = [[] for _ in range(n_meas)]
 wavelengths = [[] for _ in range(n_meas)]
 for ii, title in enumerate(data_title):
-    file = open(data_folder_full / (title + suffix['metadata']), 'r')
+    file = open(data_folder_full / (title + suffix["metadata"]), "r")
     json_metadata = json.load(file)[3]
     file.close()
-    patterns[ii] = ast.literal_eval(json_metadata['patterns'])
-    wavelengths[ii] = ast.literal_eval(json_metadata['wavelengths'])
-    
+    patterns[ii] = ast.literal_eval(json_metadata["patterns"])
+    wavelengths[ii] = ast.literal_eval(json_metadata["wavelengths"])
+
 
 # %%
 # Reorder measurements to match with the reconstruction order
 # ====================================================================
 acq_size = img_size // subsampling_factor
 Ord_acq = [
-    (-np.array(patterns[i])[::2]//2).reshape((acq_size, acq_size))
+    (-np.array(patterns[i])[::2] // 2).reshape((acq_size, acq_size))
     for i in range(n_meas)
 ]
 
@@ -104,38 +101,37 @@ Ord_acq = [
 Perm_rec = samp.Permutation_Matrix(Ord_rec)
 Perm_acq = [samp.Permutation_Matrix(Ord_acq[i]).T for i in range(n_meas)]
 # each element of 'measurements' has shape (measurements, wavelengths)
-measurements = [
-    samp.reorder(exp_data[i], Perm_acq[i], Perm_rec) for i in range(n_meas)
-]
+measurements = [samp.reorder(exp_data[i], Perm_acq[i], Perm_rec) for i in range(n_meas)]
 
 
 # %%
 # Reconstruct using a single spectral slice
 # ====================================================================
 lambda_select = 579.0970
-measurements_slice = [np.zeros((2*M, 1)) for _ in range(n_meas)]
+measurements_slice = [np.zeros((2 * M, 1)) for _ in range(n_meas)]
 
 # select the measurements in the right spectral slice
 for i in range(n_meas):
-    
+
     lambda_index = wavelengths[i].index(lambda_select)
     # take only the first 2*M measurements of the right wavelength
-    measurements_slice[i] = measurements[i][:2*M, lambda_index].reshape((1, 2*M))
-    measurements_slice[i] = torch.from_numpy(
-        measurements_slice[i]).to(device, dtype=torch.float32)
+    measurements_slice[i] = measurements[i][: 2 * M, lambda_index].reshape((1, 2 * M))
+    measurements_slice[i] = torch.from_numpy(measurements_slice[i]).to(
+        device, dtype=torch.float32
+    )
 
 
-# %% 
+# %%
 # Pinv - PnP
 # ====================================================================
 model_name = "drunet_gray.pth"
 
 # in order: USAF target, starsector, tomato x12, tomato x2
 nu_list = [
-    [20, 30, 40], # USAF target
-    [35, 45, 55], # starsector
-    [30, 40, 50], # tomato x12
-    [30, 40, 50] # tomato x2
+    [20, 30, 40],  # USAF target
+    [35, 45, 55],  # starsector
+    [30, 40, 50],  # tomato x12
+    [30, 40, 50],  # tomato x2
 ]
 
 # Initialize network
@@ -143,10 +139,10 @@ denoi = drunet.DRUNet()
 pinvnet = recon.PinvNet(noise_op, prep_op, denoi)
 pinvnet.eval()
 
-#load_net(model_folder_full / model_name, pinvnet, device, False)
+# load_net(model_folder_full / model_name, pinvnet, device, False)
 pinvnet.denoi.load_state_dict(
-    torch.load(model_folder_full / model_name, weights_only=True), 
-    strict=False)
+    torch.load(model_folder_full / model_name, weights_only=True), strict=False
+)
 pinvnet.denoi.eval()
 pinvnet = pinvnet.to(device)
 
@@ -154,15 +150,19 @@ with torch.no_grad():
     for ii, y in enumerate(measurements_slice):
         # set noise level for measurement operator and PnP denoiser
         pinvnet.prep.set_expe()
-        
+
         # iterate over noise levels
         for nu in nu_list[ii]:
             pinvnet.denoi.set_noise_level(nu)
             x_pinvnet = pinvnet.reconstruct_expe(y)
-            
-            filename = f'{data_title[ii]}_{M}_{img_size}_pinv-net_drunet_nlevel_{nu}.png'
+
+            filename = (
+                f"{data_title[ii]}_{M}_{img_size}_pinv-net_drunet_nlevel_{nu}.png"
+            )
             full_path = recon_folder_full / filename
-            plt.imsave(full_path, x_pinvnet[0, 0, :, :].cpu().detach().numpy(), cmap='gray')
-       
+            plt.imsave(
+                full_path, x_pinvnet[0, 0, :, :].cpu().detach().numpy(), cmap="gray"
+            )
+
 
 # %%
