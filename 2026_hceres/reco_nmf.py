33b7d2ec851e1d8f1e801c8b1765d751ba5357ab
COHEN Jeremy <jeremy.cohen@cnrs.fr>
2025-12-11 18:19:12 +0100

    merge commit

--- 2026_hceres/reco_nmf.py
+++ 2026_hceres/reco_nmf.py
@@ -27,73 +27,51 @@ wavelengths = ast.literal_eval(json_metadata["wavelengths"])
 
 
 # Permutation of measurements
-from spyrit.misc import sampling as samp
-subsampling_factor = 1
+# replace "np.int32(" with an empty string and ")" with an empty string
+tmp = json_metadata["patterns"]
+tmp = tmp.replace("np.int32(", "").replace(")", "")
+patterns = ast.literal_eval(tmp)  # the list (of list of) of pattern indices (evaluation because stored as text)
+wavelengths = ast.literal_eval(json_metadata["wavelengths"])
+
+# Permutation of measurements, that are acquired in an experiment-specific order
 img_size = 64
-
-acq_size = img_size // subsampling_factor
+acq_size = img_size
 Ord_acq = (-np.array(patterns)[::2] // 2).reshape((acq_size, acq_size))
-
-# Measurement and noise operators
 Ord_rec = torch.ones(img_size, img_size)
-
-# %%
-# Define the two permutation matrices used to reorder the measurements
-# measurement order -> natural order -> reconstruction order
 Perm_rec = samp.Permutation_Matrix(Ord_rec)
 Perm_acq = samp.Permutation_Matrix(Ord_acq).T
-# each element of 'measurements' has shape (measurements, wavelengths)
 Ymeas = samp.reorder(Ymeas, Perm_acq, Perm_rec)
 
-print("Shape of measurements tensor (wavelengths x measurements): ", Ymeas.shape)
-# Plotting the measurements summed over wavelengths
-Ymeas_sum_pos = np.sum(Ymeas[::2,:], axis=1)
-Ymeas_sum_neg = np.sum(Ymeas[1::2,:], axis=1)
-plt.imshow(Ymeas_sum_neg.reshape((acq_size, acq_size)), cmap='gray')
-plt.title("Sum of measurements over wavelengths (negative patterns)")
-plt.colorbar()
-plt.axis('off')
-plt.show()
-
 # Post-processing of the measurements
-# Make a wavelength x measurements tensor
 Y = torch.tensor(Ymeas, dtype=torch.float32).T
-#del Ymeas#, Ymeas_perm  # free memory
+del Ymeas
 
 # Unbiais by removing the dark current, estimated as the min value of marginals of Y (better?)
-#dc = tl.min(tl.sum(Y, axis=0))/Y.shape[0]
 dc = tl.sum(Y[:,1])/Y.shape[0] # average of dc over all wavelengths
 print(f"Estimated dark current to remove: {dc}")
 Y = Y - dc
-#print(Y[:5,:5])  # show a small part of Y
-# clip negative values
 Y = tl.clip(Y, 0, tl.max(Y))
 # Normalization to [0,1]
 Y = Y / tl.max(Y)
 
-# Showing the measurements after dark current removal
-plt.imshow(Y.cpu().numpy(), aspect='auto', cmap='gray')
-plt.title("Measurements after dark current removal")
-plt.xlabel("Pattern index")
-plt.ylabel("Wavelength index")
-plt.colorbar()
-plt.show()
+## Showing the measurements after dark current removal
+#plt.imshow(Y.cpu().numpy(), aspect='auto', cmap='gray')
+#plt.title("Measurements after dark current removal")
+#plt.xlabel("Pattern index")
+#plt.ylabel("Wavelength index")
+#plt.colorbar()
+#plt.show()
 
 ## Classic recon
 
-
-n=64
-
-# PAtterns are acquired in order Acq, compared to order nat
-# Patterns are stored in order Rec in spyrit
-acq_size = n
-meas_op = HadamSplit2d(n)
+# Reconstruction with pseudo-inverse
+acq_size = img_size
+meas_op = HadamSplit2d(img_size)
 A = meas_op.A  # noiseless operator
 Anz = torch.cat([A[0:1,:], A[2:,:]], dim=0)
 print(Anz.shape)
 
-# Remove row of zero
-# Remove 16 first bands that are null
+# Remove row of zero and remove 16 first bands that are null
 nbremove = 16
 Ynz = torch.cat([Y[nbremove:,0:1], Y[nbremove:,2:]], dim=1)
 wavelengths_nz = wavelengths[nbremove:]
@@ -113,56 +91,30 @@ Ynz = Ynz_binned
 wavelengths_nz = wavelengths_binned
 print(f"Binned measurements shape: {Ynz.shape}")
 
-# useful for algorithm
-#sumH = torch.ones((Ynz.shape[0], 2*n**2-1))@Anz  # (H.T @ 1).T
-#print(sumH.shape)
-
 # define custom foward and adjoint forward functions
 def forward(x):
     # x@Anz.T or Anz@x
     # x is of shape (B, N**2)  # batch first
     # reshape to (B, N, N)
-    temp = x.reshape((x.shape[0], n, n))  # Whyyyyyyy >????
+    temp = x.reshape((x.shape[0], img_size, img_size))  # Whyyyyyyy >????
     temp = meas_op.forward(temp).T  # shape (M, B)
     # Removing the zero row of A changes the forward A@X
     return torch.cat([temp[0:1,:], temp[2:,:]], dim=0)
 def adjoint(y):
-    # y is of shape (B, M)
-    # need to add a zero column in y, after the first column
-    temp = torch.cat([y[:, 0:1], torch.zeros((y.shape[0], 1)), y[:, 1:]], dim=1)  # add zero column
-    temp = meas_op.adjoint(temp)  # shape (B, M)
-    return temp
-def adjoint_faster(y):
-    # sad :(
+    # Also implemented as a contraction in spyrit
     return y@Anz
-#sumH = torch.ones()
 
-# Pseudo-inverse reconstruction
+
+# Pseudo-inverse reconstruction, the NNLS is quite slow here
 X_rec = torch.linalg.lstsq(Anz, Ynz.T).solution.T
-#X_rec = hals_nnls(Anz.T@Ynz.T, Anz.T@Anz, n_iter_max=10, epsilon=1e-8).T  # too slow
 
-print(Ynz.shape)  # should be (num_wavelengths, n^2)
-print(X_rec.shape)  # should be (num_wavelengths, n^2)
+# Show cat reconstructed with all wavelengths
+plt.figure(figsize=(8,6))
+plt.imshow(np.rot90(torch.sum(X_rec, dim=0).reshape(64,64), 2), cmap='gray')
+plt.title("Reconstructed image at all wavelengths")
+plt.colorbar()
+plt.axis('off')
 
-# show hypercube at some wavelengthsplt.figure(figsize=(12,6))
-for i, wl_idx in enumerate([1, 10, 150, 250]):
-    plt.subplot(2,2,i+1)
-    # pivot by 180 degrees
-    plt.imshow(X_rec[wl_idx,:].reshape(64,64), cmap='gray')
-    plt.title(f"Reconstructed image at wl index {wl_idx} ({wavelengths[wl_idx]:.1f} nm)")
-    plt.colorbar()
-    plt.axis('off')
-    
-# show some recons spectra
-plot_idx = [0, 1000, 2000, 3000]
-plt.figure(figsize=(12,6))
-for i, px_idx in enumerate(plot_idx):
-    plt.subplot(2,2,i+1)
-    plt.plot(X_rec[:,px_idx].cpu().numpy())
-    plt.title(f"Reconstructed spectrum at pixel index {px_idx}")
-    plt.xlabel("Wavelength index")
-    plt.ylabel("Intensity")
-    
 plt.show()
 
 ## NMF-based reconstruction
@@ -171,51 +123,66 @@ print("------- Starting NMF-based reconstruction --------")
 # Running the NMF-based unmixing on the reconstructed hypercube
 # Init pinv+spa
 rank = 3
-#Kset, W0, A0 = spa(X_rec, rank)
 Kset, W0, A0 = snpa(X_rec, rank, verbose=True)
 
 # Reconstruction with NMF
-lmbd = 1e-3#[1e-4, 0]
-#W_est, A_est, crit = MU_SinglePixel(Ynz, Anz, tl.abs(A0), tl.abs(W0), lmbd=lmbd, maxA=1, niter=10, n_iter_inner=20, eps=1e-8, verbose=True, print_it=1)  # regularization just for implicit scaling
-W_est, A_est, crit = MU_SinglePixel_fast(Ynz, forward, adjoint_faster, tl.abs(A0), tl.abs(W0), lmbd=lmbd, maxA=1, niter=100, n_iter_inner=20, eps=1e-8, verbose=True, print_it=20)  # regularization just for implicit scaling
-# Normalization of W_est and A_est
-#sum_W_est = torch.sum(W_est, dim=0, keepdim=True)
-#W_est = W_est/sum_W_est
-#A_est = A_est*sum_W_est.T
+lmbd = 1e-4
+W_est, A_est, crit = MU_SinglePixel_fast(Ynz, forward, adjoint, tl.abs(A0), tl.abs(W0), lmbd=lmbd, maxA=1, niter=120, n_iter_inner=20, eps=1e-8, verbose=True, print_it=40)  # regularization just for implicit scaling
 print("Computation done")
 
-# Plot the sum of the two maps, to see if they cover the whole image
-plt.imshow(torch.sum(A_est, axis=0).reshape(n,n), cmap='gray')
-plt.title("Sum of the two estimated abundance maps")
-plt.colorbar()
-plt.axis('off')
+
+Anorms = [torch.max(A_est[i,:]) for i in range(rank)]
+A0norms = [torch.max(A0[i,:]) for i in range(rank)]
+
+# show hypercube at some wavelengths and some spectra
+plt.figure(figsize=(10,10))
+for i in range(rank):
+    plt.subplot(rank,3,3*i+1)
+    plt.imshow(np.rot90(A_est[i,:].reshape(64,64), 2), cmap='gray')
+    plt.title(f"Abundance map AMU comp. {i+1}")
+    plt.colorbar(fraction=0.046, pad=0.04)
+    plt.axis('off')
+    plt.subplot(rank,3,3*i+2)
+    plt.plot(wavelengths_nz, A0norms[i]*W0[:,i].cpu().numpy())
+    plt.plot(wavelengths_nz, Anorms[i]*W_est[:,i].cpu().numpy())
+    plt.title(f"Spectrum comp. {i+1}")
+    plt.xlabel("Wavelength index")
+    plt.ylabel("Intensity") 
+    plt.legend(["pinv+SNPA", "AMU"])
+    plt.subplot(rank,3,3*i+3)
+    plt.imshow(np.rot90(A0[i,:].reshape(64,64), 2), cmap='gray')
+    plt.title(f"Abundance map init comp. {i+1}")
+    plt.colorbar(fraction=0.046, pad=0.04)
+    plt.axis('off')
+    plt.tight_layout()
 plt.show()
 
-# Superimpose the four estimated abundance maps in an RBG image with four different colors
-abundance_map_rgb = torch.zeros((n, n, 3))
-# First component in red
-abundance_map_rgb[:, :, 0] += A_est[0,:].reshape(n,n)
-# Second component in green
-abundance_map_rgb[:, :, 1] += A_est[1,:].reshape(n,n)
-# Third component in blue
-abundance_map_rgb[:, :, 2] += A_est[2,:].reshape(n,n)
-if rank > 3:
-    # Fourth component in yellow (red+green)
-    abundance_map_rgb[:, :, 0] += A_est[3,:].reshape(n,n)
-    abundance_map_rgb[:, :, 1] += A_est[3,:].reshape(n,n)
-    if rank > 4:
-        # use magenta for the fifth component (red+blue)
-        abundance_map_rgb[:,:,0] += A_est[4,:].reshape(n,n)
-        abundance_map_rgb[:,:,2] += A_est[4,:].reshape(n,n)
-        if rank > 5:
-            # use cyan for the sixth component (green+blue)
-            abundance_map_rgb[:,:,1] += A_est[5,:].reshape(n,n)
-            abundance_map_rgb[:,:,2] += A_est[5,:].reshape(n,n)
-# Clip values to [0,1]
-abundance_map_rgb = torch.clamp(abundance_map_rgb, 0, 1)
-plt.imshow(abundance_map_rgb.cpu().numpy())
-plt.title("Superimposed estimated abundance maps")
-plt.axis('off')
-# add color legends
-plt.legend(['Component 1 (Red)', 'Component 2 (Green)', 'Component 3 (Blue)', 'Component 4 (Yellow)', 'Component 5 (Magenta)', 'Component 6 (Cyan)'][:rank], loc='upper right')
-plt.show()
+
+## Superimpose the four estimated abundance maps in an RBG image with four different colors
+#abundance_map_rgb = torch.zeros((n, n, 3))
+## First component in red
+#abundance_map_rgb[:, :, 0] += A_est[0,:].reshape(n,n)
+## Second component in green
+#abundance_map_rgb[:, :, 1] += A_est[1,:].reshape(n,n)
+## Third component in blue
+#abundance_map_rgb[:, :, 2] += A_est[2,:].reshape(n,n)
+#if rank > 3:
+    ## Fourth component in yellow (red+green)
+    #abundance_map_rgb[:, :, 0] += A_est[3,:].reshape(n,n)
+    #abundance_map_rgb[:, :, 1] += A_est[3,:].reshape(n,n)
+    #if rank > 4:
+        ## use magenta for the fifth component (red+blue)
+        #abundance_map_rgb[:,:,0] += A_est[4,:].reshape(n,n)
+        #abundance_map_rgb[:,:,2] += A_est[4,:].reshape(n,n)
+        #if rank > 5:
+            ## use cyan for the sixth component (green+blue)
+            #abundance_map_rgb[:,:,1] += A_est[5,:].reshape(n,n)
+            #abundance_map_rgb[:,:,2] += A_est[5,:].reshape(n,n)
+## Clip values to [0,1]
+#abundance_map_rgb = torch.clamp(abundance_map_rgb, 0, 1)
+#plt.imshow(abundance_map_rgb.cpu().numpy())
+#plt.title("Superimposed estimated abundance maps")
+#plt.axis('off')
+## add color legends
+#plt.legend(['Component 1 (Red)', 'Component 2 (Green)', 'Component 3 (Blue)', 'Component 4 (Yellow)', 'Component 5 (Magenta)', 'Component 6 (Cyan)'][:rank], loc='upper right')
+#plt.show()
